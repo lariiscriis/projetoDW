3f67a9d7c919d9230f46506cf6be8e8d3dd2e8d2
lariiscriis <larissa.santana.ae2020@gmail.com>
2025-05-16 10:15:10 -0300

    Verificação de Sessão, link no forms Cadastro, Login (usuário), links no dashboard

--- app/urls.py
+++ app/urls.py
@@ -7,9 +7,7 @@ urlpatterns = [
     path('', views.index, name = "app"),
     path('usuarios', views.exibirUsuarios , name = "exibirUsuarios"),
     path('login', views.login , name = "login"),
-
     path('dashboard', views.dashboard , name = "dashboard"),
-
     path('add-usuario', views.addUsuario, name="addUsuario"),
     path('excluir-usuario/<int:id_usuario>', views.excluirUsuario, name="excluirUsuario" ),
     path('editar-usuario/<int:id_usuario>', views.editarUsuario, name="editarUsuario"),
--- app/views.py
+++ app/views.py
@@ -7,12 +7,16 @@ from app.forms import formUsuario, formProduto, formLogin
 # Create your views here.
 
 def exibirUsuarios(request):
+    if not request.session.get("email"):
+        return redirect("app")
+
     usuarios = Usuario.objects.all().values()
     return render(request, "usuarios.html", {'listUsuarios': usuarios})
 
 def index(request):
     template = loader.get_template("index.html")
     return HttpResponse(template.render())
+
 
 def login(request):
     frmLogin = formLogin(request.POST or None)
@@ -23,10 +27,10 @@ def login(request):
             try:
                 userLogin = Usuario.objects.get(email=_email,senha= _senha)
                 if userLogin is not None:
-                    request.session.set_expiry(timedelta(seconds=30))
+                    request.session.set_expiry(timedelta(seconds=600))
                    
                     request.session['email'] = _email
-                    tempo_sessao = timedelta(seconds=30)
+                    tempo_sessao = timedelta(seconds=600)
                     tempo_sessao_segundos = tempo_sessao.total_seconds()
                     request.session['tempo_sessao_segundos '] =  tempo_sessao_segundos
                     return redirect("dashboard")
@@ -34,19 +38,18 @@ def login(request):
                 return render(request, "login.html")
     return render(request, "login.html", {'form': frmLogin})
 
+
 def dashboard(request):
     _email = request.session.get("email")
     tempo_sessao = request.session.get("tempo_sessao_segundos")
     if _email is None:
             return render(request, "index.html")
-    if tempo_sessao and tempo_sessao > timedelta(seconds=60) :
+    if tempo_sessao and tempo_sessao > timedelta(seconds=600) :
         return render(request, "index.html")
     else:
         return render(request, "dashboard.html", {'email' : _email})
 
         
-    
-
 def addUsuario(request):
     formUser = formUsuario(request.POST or None)
     if request.POST:
@@ -56,10 +59,12 @@ def addUsuario(request):
         
     return render(request, "add-usuario.html", {'form':formUser})
 
+
 def excluirUsuario(request, id_usuario):
     usuario = Usuario.objects.get(id=id_usuario)
     usuario.delete()
     return redirect("exibirUsuarios")
+
 
 def editarUsuario(request, id_usuario):
     usuario = Usuario.objects.get(id=id_usuario)
@@ -72,7 +77,11 @@ def editarUsuario(request, id_usuario):
         
     return render(request, "editar-usuario.html", {'form': formUser})
 
+
 def cadastrarProduto(request):
+    if not request.session.get("email"):
+        return redirect("app")
+
     if request.method == 'POST':
         formProduct = formProduto(request.POST, request.FILES)
         if formProduct.is_valid():
@@ -83,13 +92,18 @@ def cadastrarProduto(request):
 
 
 def listarProdutos(request):
+    if not request.session.get("email"):
+        return redirect("app")
+
     produtos = Produto.objects.all().values()
     return render(request, "listar-produtos.html", {'listProdutos': produtos})
+
 
 def excluirProduto(request, id_produto):
     produto = Produto.objects.get(id=id_produto)
     produto.delete()
     return redirect("listarProdutos")
+
 
 def editarProduto(request, id_produto):
     produto = Produto.objects.get(id=id_produto)
@@ -101,6 +115,7 @@ def editarProduto(request, id_produto):
             return redirect("listarProdutos")
         
     return render(request, "editar-produto.html", {'form': formProd})
+
 
 def cardsProdutos(request):
     produtos = Produto.objects.all().values()
